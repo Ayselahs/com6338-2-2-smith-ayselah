add518b46e169d8448034735d74f2dd010aa61f5
Ayselah <ayselahs@gmail.com>
2023-05-25 17:21:28 -0500

    Edited index.js to create greet function

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Your code here
+function greet() {
+    let name = prompt("What is your name?");
+    alert("Hello, "  + name);
+    let age = prompt("How old are you?");
+    age = parseInt(age);
+    let conBirthday = confirm("Did you have your birthday this year yet?");
+    currentYear = new Date().getFullYear();
+    if (conBirthday === true) {
+            birthday = currentYear - age;
+            alert("Your birth year is " + birthday);
+    } else {
+            birth = currentYear - (age + 1);
+            alert("Your birth year is " + birth);
+    }
+}
+
